4223e8cfdad0e80dd3a409ecc51fed88268726c9
Derek Hogue <derek@amphibian.info>
2011-07-07 13:39:30 -0500

    Added 'None' option to display_field(); default Entry Type option

--- views/option_row.php
+++ views/option_row.php
@@ -1,5 +1,6 @@
 		<tr>
 			<td><?=form_input(sprintf('entry_type_options[%s][type]', $i), $type)?></td>
 			<td><?=form_multiselect(sprintf('entry_type_options[%s][hide_fields][]', $i), $fields, $hide_fields)?></td>
+			<td><label><?=form_radio('entry_type_default_type', $type, ($type == $default_type) ? TRUE : FALSE)?> <?=lang('yes')?></label></td>
 			<td><a href="javascript:void(0);" class="entry_type_remove_row"><?=img(array('border' => '0', 'src' => $this->config->item('theme_folder_url').'cp_themes/default/images/content_custom_tab_delete.png'))?></a></td>
 		</tr>
