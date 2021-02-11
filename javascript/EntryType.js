8c357c2f3ddaf1b1f91033bc31115866656c1255
Brian Litzinger <litzinger@gmail.com>
2021-02-11 12:24:16 -0600

    Fix for gridHoldFields, and also account for React based fields

--- javascript/EntryType.js
+++ javascript/EntryType.js
@@ -23,32 +23,57 @@
     var $tabs = $(".tab-wrap .tabs").find("li");
 
     // add grid fields
-    // var $gridHoldFields = $(".fieldset-faux").filter(function() {
-    //   var match = $(this).find(".grid-input-form").attr("id").match(/^field_id_(\d+)$/);
-    //
-    //   if (match) {
-    //     $(this).addClass('entry-type-field-' + match[1]);
-    //
-    //     return true;
-    //   }
-    //
-    //   return false;
-    // });
+    var $gridHoldFields = $('.fieldset-faux').filter(function() {
+        var $gridField = $(this).find('.grid-field');
 
-    // $holdFields = $holdFields.add($gridHoldFields);
+        if ($gridField.length) {
+            var match = $gridField.attr('id').match(/^field_id_(\d+)$/);
+
+            if (match) {
+                $(this).addClass('entry-type-field-' + match[1]);
+
+                return true;
+            }
+        }
+
+        return false;
+    });
+
+    // Selector is very specific b/c the <fieldset> on React fields has no class, and we don't want to select
+    // a field inside of a Grid or Bloqs field on accident. We can't use .filter() b/c we need to find the child
+    // field, then find it's parent fieldset, which we append classes to.
+    var $reactHoldFields = [];
+    $('.tab > fieldset > .field-instruct + .field-control > div[data-input-value]').each(function () {
+        var $field = $(this);
+        var $fieldset = $field.closest('fieldset');
+        var match = $field.attr('data-input-value').match(/^field_id_(\d+)/);
+
+        if (match) {
+            $fieldset
+                .addClass('entry-type-field-' + match[1]);
+
+            $reactHoldFields.push($fieldset);
+        }
+    });
+
+    $holdFields = $holdFields
+        .add($gridHoldFields)
+        .add($reactHoldFields)
+    ;
 
     w.EntryType = {
         fields: [],
         change: function(event) {
-            var i, j, $input, value, fieldId;
+            var value;
+
             $holdFields.not(this).each(function() {
                 var $this = $(this);
                 if (!$this.data("invisible")) {
                     $this.removeClass("entry-type-hidden");
                 }
             });
-            for (i = 0; i < EntryType.fields.length; i++) {
 
+            for (var i = 0; i < EntryType.fields.length; i++) {
                 if (EntryType.fields[i].callback !== null) {
                     value = $.proxy(EntryType.fields[i].callback, EntryType.fields[i])();
 
@@ -59,10 +84,11 @@
                     value = (EntryType.fields[i].$input.is(":radio")) ? EntryType.fields[i].$input.filter(":checked").val() : EntryType.fields[i].$input.val();
                 }
 
-                for (fieldId in EntryType.fields[i].hideFields[value]) {
+                for (var fieldId in EntryType.fields[i].hideFields[value]) {
                     $(".entry-type-field-" + EntryType.fields[i].hideFields[value][fieldId]).addClass("entry-type-hidden");
                 }
             }
+
             $tabs.each(function() {
                 var tabNum = $(this).find("a").attr("rel"),
                     $tab = $(this),
